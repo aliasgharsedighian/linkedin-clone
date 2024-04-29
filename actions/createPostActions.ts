372090dc31070f09ed2937725561ae76f0743a29
Ali asghar sedighian job <as.sedighian@gmail.com>
2024-04-29 17:15:03 +0330

    ready to upload image to link

--- actions/createPostActions.ts
+++ actions/createPostActions.ts
@@ -4,6 +4,7 @@ import { AddPostRequestBody } from "@/app/api/posts/route";
 import { Post } from "@/mongodb/models/Post";
 import { IUser } from "@/types/user";
 import { currentUser } from "@clerk/nextjs/server";
+import { randomUUID } from "crypto";
 
 export default async function createPostAction(formData: FormData) {
   const user = await currentUser();
@@ -13,7 +14,7 @@ export default async function createPostAction(formData: FormData) {
 
   const postInput = formData.get("postInput") as string;
   const image = formData.get("image") as File;
-  let imageUrl: string | undefined;
+  let image_url: string | undefined = undefined;
 
   if (!postInput) {
     throw new Error("Post input is required");
@@ -28,12 +29,34 @@ export default async function createPostAction(formData: FormData) {
   };
   try {
     if (image.size > 0) {
-      //1.upload image if there is one with image
-      //2.create post in database
+      //1.upload image if there is one
+
+      const timestamp = new Date().getTime();
+      const file_name = `${randomUUID()}_${timestamp}.png`;
+
+      const imageBuffer: any = await image.arrayBuffer();
+
+      const formdata = new FormData();
+      formdata.append("key", "700a99941c1160d98138d5e03458c711");
+      formdata.append("image", imageBuffer);
+
+      const requestOptions = {
+        method: "POST",
+        body: formdata,
+      };
+
+      fetch("https://api.imgbb.com/1/upload", requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+          image_url = result.data.url;
+        })
+        .catch((error) => console.error(error));
+
+      //2.create post in database with image
       const body: AddPostRequestBody = {
         user: userDB,
         text: postInput,
-        // imageUrl: image_url,
+        imageUrl: "https://i.ibb.co/0Xbbbzz/OIG.jpg",
       };
 
       await Post.create(body);
@@ -49,7 +72,6 @@ export default async function createPostAction(formData: FormData) {
   } catch (error: any) {
     throw new Error("Failed to create post", error);
   }
-  //create post in database
 
   //revalidatePath "/" - home page
 }
