c56726b84f6d1bfc697417f1fa11f767d640ec61
Ali asghar sedighian job <as.sedighian@gmail.com>
2024-05-01 14:15:09 +0330

    seperate comment author

--- components/CommentFeed.tsx
+++ components/CommentFeed.tsx
@@ -8,12 +8,19 @@ import ReactTimeago from "react-timeago";
 function CommentFeed({ post }: { post: IPostDocument }) {
   const { user } = useUser();
 
-  const isAuthor = user?.id === post.user.userId;
+  const isAuthor = user?.id === post.user?.userId;
 
   return (
     <div className="space-y-2 mt-3">
       {post.comments?.map((comment) => (
-        <div key={comment._id} className="flex space-x-1">
+        <div
+          key={comment._id}
+          className={`flex space-x-1 ${
+            comment.user.userId === user?.id
+              ? "flex-row-reverse"
+              : "flex-row justify-start"
+          }`}
+        >
           <Avatar>
             <AvatarImage src={comment.user?.userImage} />
             <AvatarFallback>
@@ -22,11 +29,15 @@ function CommentFeed({ post }: { post: IPostDocument }) {
             </AvatarFallback>
           </Avatar>
 
-          <div className="bg-gray-100 px-4 py-2 rounded-md w-full sm:w-auto md:min-w-[300px]">
+          <div
+            className={`px-4 py-2 rounded-md w-full sm:w-auto md:min-w-[300px] ${
+              comment.user.userId === user?.id ? "bg-sky-100" : "bg-slate-100"
+            }`}
+          >
             <div className="flex justify-between">
               <div>
                 <p className="font-semibold">
-                  {comment.user.firstName} {comment.user?.lastName}
+                  {comment.user.firstName} {comment.user?.lastName}{" "}
                 </p>
                 <p className="text-xs text-gray-400">
                   @{comment.user.firstName}
@@ -34,9 +45,11 @@ function CommentFeed({ post }: { post: IPostDocument }) {
                   {comment.user.userId.toString().slice(-4)}
                 </p>
               </div>
-              <p className="text-xs text-gray-400">
-                <ReactTimeago date={new Date(comment.createdAt)} />
-              </p>
+              <div className="flex flex-col">
+                <p className="text-xs text-gray-400">
+                  <ReactTimeago date={new Date(comment.createdAt)} />
+                </p>
+              </div>
             </div>
             <p className="mt-3 text-sm">{comment.text}</p>
           </div>
