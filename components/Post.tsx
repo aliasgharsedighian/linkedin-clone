570e41d0a052529cc600bf5d85f6a7c3507bf9e4
Ali Asghar Sedighian <as.sedighian@gmail.com>
2024-05-03 15:05:13 +0330

    create update posts action

--- components/Post.tsx
+++ components/Post.tsx
@@ -2,12 +2,12 @@
 
 import { IPostDocument } from "@/mongodb/models/Post";
 import { useUser } from "@clerk/nextjs";
-import React from "react";
+import React, { useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Badge } from "./ui/badge";
 import ReactTimeago from "react-timeago";
 import { Button } from "./ui/button";
-import { Edit2Icon, Trash2 } from "lucide-react";
+import { Edit2Icon, Send, Trash2 } from "lucide-react";
 import deletePostAction from "@/actions/deletePostAction";
 import Image from "next/image";
 import PostOptions from "./PostOptions";
@@ -19,6 +19,8 @@ function Post({ post }: { post: IPostDocument }) {
   const { user } = useUser();
 
   const [isPending, startTransition] = useTransition();
+  const [isPendingEdit, startTransitionEdit] = useTransition();
+  const [edit, setEdit] = useState(false);
 
   const isAuthor = user?.id === post.user?.userId;
 
@@ -39,11 +41,14 @@ function Post({ post }: { post: IPostDocument }) {
 
   const handleUpdatePostAction = async (postId: string, editText: string) => {
     try {
-      const promise = editPostAction(postId, editText);
-      toast.promise(promise, {
-        loading: "Updating post...",
-        success: "Post updated",
-        error: "Failed to update post",
+      startTransitionEdit(async () => {
+        const promise = editPostAction(postId, editText);
+        toast.promise(promise, {
+          loading: "Updating post...",
+          success: "Post updated",
+          error: "Failed to update post",
+        });
+        setEdit(false);
       });
     } catch (error) {
       console.log("Error Deleting post:", error);
@@ -84,9 +89,10 @@ function Post({ post }: { post: IPostDocument }) {
           {isAuthor && (
             <div className="flex items-start gap-2">
               <Button
+                variant={edit ? "default" : "ghost"}
                 className="px-3 py-2"
                 onClick={() => {
-                  handleUpdatePostAction(post._id, "dsjksdkj");
+                  setEdit(!edit);
                 }}
               >
                 <Edit2Icon size={16} />
@@ -130,7 +136,59 @@ function Post({ post }: { post: IPostDocument }) {
       </div>
 
       <div>
-        <p className="px-4 pb-2 mt-2">{post.text}</p>
+        {edit ? (
+          <form
+            action={(formData) => {
+              const editInput = formData.get("editInput") as string;
+              if (!editInput.trim()) {
+                toast.error("You must provide a comment input");
+              } else {
+                handleUpdatePostAction(post._id, editInput);
+              }
+            }}
+          >
+            <div className="flex flex-1 bg-white border rounded-full px-3 py-2 mx-3 mb-3">
+              <input
+                className="outline-none flex-1 text-sm bg-transparent"
+                type="text"
+                defaultValue={post.text}
+                name="editInput"
+                placeholder="Edit your text..."
+              />
+              <button
+                type="submit"
+                aria-disabled={isPendingEdit}
+                disabled={isPendingEdit}
+              >
+                {isPendingEdit ? (
+                  <div className="mr-2" role="status">
+                    <svg
+                      aria-hidden="true"
+                      className="w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentFill"
+                      />
+                    </svg>
+                    <span className="sr-only">Loading...</span>
+                  </div>
+                ) : (
+                  <Send size={20} className="mr-2" color="#4881c2" />
+                )}
+              </button>
+            </div>
+          </form>
+        ) : (
+          <p className="px-4 pb-2 mt-2">{post.text}</p>
+        )}
         {/* If image uploaded put it here ... */}
         {post.imageUrl && (
           <Image
